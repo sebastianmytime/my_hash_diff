06bf3a7ccad3300eb8b91ac5e28d375a49c99c86
sebastian_lascau <sebastian@mytime.com>
2022-09-14 13:25:10 +0300

    using refine to avoid collisions

--- lib/my_hash_diff.rb
+++ lib/my_hash_diff.rb
@@ -1,8 +1,11 @@
-class Hash
-  def my_diff(hash)
-    hash.each do |key, value|
-      self.delete_if {|k, v| k == key and v == value}
+
+module SebastianHash
+  refine Hash do
+    def my_diff(hash)
+      hash.each do |key, value|
+        self.delete_if {|k, v| k == key and v == value}
+      end
+      self
     end
-    self
   end
 end
